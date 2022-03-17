d304b104cb5e3f24086da94e356673057a186765
J Ponia <jponiatowski@hotmail.fr>
2022-03-17 13:15:17 +0100

    home logic js

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,34 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    console.log(data)
+    const { name, portrait, city, country, tagline, id, price} = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `../../assets/photographers/id/${portrait}`;
+    const firstname = name.split(' ')[0];
+    
 
     function getUserCardDOM() {
+        console.log(firstname)
         const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        article.innerHTML = `
+                            <a href="photographer.html?id=${id}">
+                            <img
+                                src="${picture}"
+                            />
+                            <h2>${name}</h2>
+                            </a>
+                            <p>${city}</p>
+                            <p>${tagline}</p>
+                            <p>${price}</p>
+                            `
+        // const img = document.createElement( 'img' );
+        // img.setAttribute("src", picture)
+        // const h2 = document.createElement( 'h2' );
+        // h2.textContent = name;
+        // article.appendChild(img);
+        // article.appendChild(h2);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, portrait, city, country, tagline, id, price, firstname, getUserCardDOM }
 }
+
+
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,13 +1,11 @@
     async function getPhotographers() {
         const data = new UserApi('../../data/photographers.json');
         const datas = await data.get();
-        // const photographers = datas.photographers;
-        // console.log(photographers);
-        // const lospo = new photographer(datas)
-        // console.log(lospo)
         const userdata = datas.photographers;
-        const photographers = userdata.map(user => new photographer(user));
-        console.log(photographers);
+        const photographers = userdata.map(user => photographerFactory(user))
+        console.log(photographers)
+        // const photographers = userdata.map(user => new photographer(user));
+        // console.log(photographers);
 
         return ({
             photographers
@@ -18,9 +16,8 @@
         const photographersSection = document.querySelector(".photographer_section");
 
         photographers.forEach((photographer) => {
-            
-            const photographerModel = new photographerCard(photographer);
-            const userCardDOM = photographerModel.createUserCard();
+            const photographerModel = photographerFactory(photographer);
+            const userCardDOM = photographerModel.getUserCardDOM();
             photographersSection.appendChild(userCardDOM);
         });
         
